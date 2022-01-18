fac0c9356d5d212f9ebae40f2e29292c7102aa21
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2022-01-18 16:51:08 +0000

    swagger api documentation

--- src/main/java/io/turntabl/leaderboardservice/LeaderboardServiceApplication.java
+++ src/main/java/io/turntabl/leaderboardservice/LeaderboardServiceApplication.java
@@ -1,15 +1,28 @@
 package io.turntabl.leaderboardservice;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.info.Info;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition
 public class LeaderboardServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeaderboardServiceApplication.class, args);
+	}
+	@Bean
+	public OpenAPI customConfiguration() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Codewars leaderboard - API Doc")
+						.description("Api Rest documentation."));
 	}
 
 }
