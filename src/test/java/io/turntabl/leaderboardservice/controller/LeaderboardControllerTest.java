852993d922485bd96636909f520d029bbb481680
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2022-01-19 15:46:32 +0000

    unit tests for fetching leaderboard by language

--- src/test/java/io/turntabl/leaderboardservice/controller/LeaderboardControllerTest.java
+++ src/test/java/io/turntabl/leaderboardservice/controller/LeaderboardControllerTest.java
@@ -2,21 +2,39 @@ package io.turntabl.leaderboardservice.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.turntabl.leaderboardservice.controller.response.ProfileDto;
+import io.turntabl.leaderboardservice.enums.Languages;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ContextConfiguration(classes = {LeaderboardController.class})
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = LeaderboardController.class)
 class LeaderboardControllerTest {
+
+    @Autowired
+    private LeaderboardController leaderboardController;
 
     @Autowired
     private MockMvc mockMvc;
@@ -40,5 +58,18 @@ class LeaderboardControllerTest {
         mockMvc.perform(get("/v1/leaderboard"))
                 .andExpect(status().isOk())
                 .andExpect(result -> assertThat(result.getResponse().getContentAsString()).isEqualTo(objectMapper.writeValueAsString(expectedResponse)));
+    }
+
+    @Test
+    void testGetLeaderboardByLanguage() throws Exception {
+        when(this.leaderboardFacade.getProfileByLanguage((String) any())).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/v1/leaderboard/{language}",
+                Languages.java);
+        MockMvcBuilders.standaloneSetup(this.leaderboardController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
     }
 }
--- src/test/java/io/turntabl/leaderboardservice/service/LeaderboardRepositoryServiceTest.java
+++ src/test/java/io/turntabl/leaderboardservice/service/LeaderboardRepositoryServiceTest.java
@@ -1,7 +1,12 @@
 package io.turntabl.leaderboardservice.service;
 
+import io.turntabl.leaderboardservice.controller.response.LanguageProfileDto;
+import io.turntabl.leaderboardservice.model.LanguageLevel;
 import io.turntabl.leaderboardservice.model.Profile;
 import io.turntabl.leaderboardservice.repository.ProfileRepository;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,16 +15,26 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ContextConfiguration(classes = {LeaderboardRepositoryService.class})
 @ExtendWith(MockitoExtension.class)
 class LeaderboardRepositoryServiceTest {
 
+
     @Mock
     private ProfileRepository profileRepository;
-
+    @Autowired
     private LeaderboardRepositoryService underTest;
 
     @BeforeEach
@@ -41,5 +56,34 @@ class LeaderboardRepositoryServiceTest {
         // then
         assertThat(result).containsExactly(profile1, profile2);
     }
+
+    @Test
+    void testGetProfileByLanguage() {
+        when(this.profileRepository.findAll()).thenReturn(new ArrayList<>());
+        List<LanguageProfileDto> actualProfileByLanguage = underTest.getProfileByLanguage("en");
+        assertTrue(actualProfileByLanguage.isEmpty());
+        verify(this.profileRepository).findAll();
+        assertEquals(actualProfileByLanguage, underTest.getProfiles());
+    }
+
+    @Test
+    void testGetProfileByLanguage2() {
+        Profile profile = new Profile();
+        profile.setClan("Clan");
+        profile.setHonour(0);
+        profile.setId("42");
+        profile.setLanguageLevels(new ArrayList<>());
+        profile.setName("Name");
+        profile.setOverallRank(0);
+
+        ArrayList<Profile> profileList = new ArrayList<>();
+        profileList.add(profile);
+        when(profileRepository.findAll()).thenReturn(profileList);
+        assertTrue(underTest.getProfileByLanguage("en").isEmpty());
+        verify(profileRepository).findAll();
+        assertEquals(1, underTest.getProfiles().size());
+    }
+
+
 
 }
